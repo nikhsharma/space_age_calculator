7664aab935c75c5eb7816dac29f4573526365da4
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-29 21:44:46 +0100

    Adds function to calculate age on mercury. Adds multiple choice input

--- space_age_calculator.py
+++ space_age_calculator.py
@@ -7,8 +7,19 @@ def print_header():
 def calculate_age_on_earth(age):
     return int(age/31557600)
 
+def calculate_age_on_mercury(age):
+    return int(calculate_age_on_earth(age)*0.2408467)
+
+# Program Start
 
 print_header()
 
 age = int(input('Enter your age in seconds: '))
-print(calculate_age_on_earth(age))
+
+print('Do you want your age on: ')
+print('1. Earth')
+print('2. Mercury')
+response = input('Please enter the corresponding number: ')
+
+if response is '1': print(calculate_age_on_earth(age))
+if response is '2': print(calculate_age_on_mercury(age))
